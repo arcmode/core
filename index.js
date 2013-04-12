479df3a6f18e610e2c1515dc7dcd2a6612af18c9
David Rojas <drojascamaggi@gmail.com>
2013-04-12 03:06:12 -0400

    Add status management

--- index.js
+++ index.js
@@ -15,30 +15,128 @@
 
 var Emitter = require('emitter');
 
+/*
+ * Private
+ */
+
+var status = 'uninitialized';
+
+/**
+ * @method joinEvents
+ * @description 
+ *   Adopt events from parent.
+ * 
+ * @api private
+ */
+var joinEvents = function(hosted, host) {
+  if (host && host.hasOwnProperty('events')) {
+    hosted.events = host.events;
+  }
+  return hosted;
+};
+
 /**
  * @constructor Core
  */
 
 var Core = function() {
-  this.modules = [];
+  this.modules = {};
   this.events = new Emitter();
 };
 
 /**
- * @method init
+ * @method status
  * @description 
- *   Call the init method of each `this.modules`.
+ *   Get the status. Possible values: `uninitialized`, `running` and `stopped`.
+ * 
+ * @return {String} the status
+ * @api public
+ */
+
+Core.prototype.status = function(){
+  return status;
+};
+
+/**
+ * @method changeStatus
+ * @description 
+ *   Change the status of this and each module.
  * 
  * @return {Core} this for chaining
  * @api public
  */
 
-Core.prototype.init = function(){
-  for (var i = modules.length - 1; i >= 0; i--){
-    modules[i].init(this);
+Core.prototype.changeStatus = function(options){
+  if (this.status() === options.when) {
+    var modules = this.modules;
+    for (var module in modules) {
+      var mod = modules[module];
+      mod[options.perform](this);
+    };
+    status = options.success;
+    return this;
+  }
+  throw new Error(options.fail);
+};
+
+/**
+ * @method init
+ * @description 
+ *   Call the init method on each module.
+ * 
+ * @return {Core} this for chaining
+ * @api public
+ */
+
+Core.prototype.init = function(parent){
+  joinEvents(this, parent);
+  var options = {
+    when: 'uninitialized',
+    perform: 'init',
+    success: 'running',
+    fail: 'Initialization can be performed only on uninitialized'
   };
-  this.events.emit('init');
-  return this;
+  this.changeStatus(options);
+};
+
+/**
+ * @method stop
+ * @description 
+ *   Call the stop method on each module.
+ * 
+ * @return {Core} this for chaining
+ * @api public
+ */
+
+Core.prototype.stop = function(){
+  joinEvents(this, parent);
+  var options = {
+    when: 'running',
+    perform: 'stop',
+    success: 'stopped',
+    fail: 'Stop can be performed only on running'
+  };
+  this.changeStatus(options);
+};
+
+/**
+ * @method resume
+ * @description 
+ *   Call the resume method on each module.
+ * 
+ * @return {Core} this for chaining
+ * @api public
+ */
+
+Core.prototype.resume = function(){
+  joinEvents(this, parent);
+  var options = {
+    when: 'stopped',
+    perform: 'resume',
+    success: 'running',
+    fail: 'Resume can be performed only on stopped'
+  };
+  this.changeStatus(options);
 };
 
 /**
@@ -50,8 +148,8 @@ Core.prototype.init = function(){
  * @api public
  */
 
-Core.prototype.use = function(module){
-  this.modules.push(module);
+Core.prototype.use = function(id, module){
+  this.modules[id] = module;
   return this;
 };
 
@@ -67,7 +165,6 @@ Core.prototype.use = function(module){
 Core.create = function(module){
   return new this();
 };
-
 
 /*
  * Expose `Core`
