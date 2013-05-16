ee76316040c7ccd4717ad5e1a110ec17f5db728f
David Rojas <drojascamaggi@gmail.com>
2013-05-16 01:56:25 -0400

    Refactor: support amd and change some event names

--- index.js
+++ index.js
@@ -1,123 +1,143 @@
 
-/**
- * Core
- * Use and initialize objects with init function property.
- * 
- * @copyright 2013 David Rojas
- * @license MIT
- */
-
-'use strict';
-
 /*
- * Module dependencies
+ * X-platform module
  */
 
-var Emitter;
+;(function (root, factory) {
 
-try {
-  Emitter = require('events').EventEmitter;
-} catch (err) {
-  Emitter = require('emitter');
-}
+  'use strict';
 
-/**
- * @constructor Core
- */
+  if (typeof exports === 'object') {
+    module.exports = factory();
 
-function Core(id){
-  this.id = id || (Math.floor(Math.random() * 256)).toString(16);
-  this.modules = {};
-  this.status = 'stopped';
-};
+  } else if (typeof define === 'function' && define.amd) {
+    define('core', factory);
 
-/*
- * Inherit from `EventEmitter.prototype` or `Emitter.prototype`.
- */
-
-Core.prototype.__proto__ = Emitter.prototype;
-
-/*
- * PubSub facade.
- */
-
-Core.prototype.public = new Emitter();
-Core.prototype.publish = function(){
-  this.public.emit.apply(this.public, arguments);
-  return this;
-};
-Core.prototype.subscribe = function(){
-  this.public.on.apply(this.public, arguments);
-  return this;
-};
-Core.prototype.subscribeOnce = function(){
-  this.public.once.apply(this.public, arguments);
-  return this;
-};
-Core.prototype.unsubscribe = function(){
-  this.public.off.apply(this.public, arguments);
-  return this;
-};
-
-/**
- * @method init
- * @description 
- *   Call the init method on each module.
- * 
- * @return {Core} this for chaining
- * @api public
- */
-
-Core.prototype.init = function(){
-  var self = this;
-  if (this.status === 'stopped') {
-    self.emit('pre init');
-    var modules = this.modules;
-    for (var module in modules) {
-      var mod = modules[module];
-      mod.init && mod.init();
-    };
-    this.status = 'running';
-    self.emit('init');
+  } else {
+    root.core = factory();
   }
-  return this;
-};
 
-/**
- * @method use
- * @description 
- *   Add a `module` to `this.modules`.
- * 
- * @return {Core} this for chaining
- * @api public
- */
+}(this, function () {
 
-Core.prototype.use = function(module){
-  var args = [].slice.call(arguments);
-  if (args.length > 1) {
-    module = args[1];
-    module.id = args[0];
+  /**
+   * Core
+   * Use and initialize objects with init function property.
+   *
+   * @copyright 2013 David Rojas
+   * @license MIT
+   */
+
+  /*
+   * Module dependencies
+   */
+
+  var Emitter;
+
+  try {
+    Emitter = require('events').EventEmitter;
+  } catch (err) {
+    Emitter = require('emitter');
   }
-  this.modules[module.id] = module;
-  return this;
-};
 
-/**
- * @method create
- * @description 
- *   Create a `Core`.
- * 
- * @return {Core} new `Core`
- * @api public
- */
+  /**
+   * @constructor Core
+   */
 
-Core.create = function(id){
-  var core = new Core(id);
-  return core;
-};
+  function Core(id){
+    this.id = id || (Math.floor(Math.random() * 256)).toString(16);
+    this.modules = {};
+    this.status = 'stopped';
+  }
 
-/*
- * Expose `Core`
- */
+  /*
+   * Inherit from `EventEmitter.prototype` or `Emitter.prototype`.
+   */
 
-module.exports = Core;
+  Core.prototype = Emitter.prototype;
+
+  /*
+   * PubSub facade.
+   */
+
+  Core.prototype.public = new Emitter();
+  Core.prototype.publish = function(){
+    this.public.emit.apply(this.public, arguments);
+    return this;
+  };
+  Core.prototype.subscribe = function(){
+    this.public.on.apply(this.public, arguments);
+    return this;
+  };
+  Core.prototype.subscribeOnce = function(){
+    this.public.once.apply(this.public, arguments);
+    return this;
+  };
+  Core.prototype.unsubscribe = function(){
+    this.public.off.apply(this.public, arguments);
+    return this;
+  };
+
+  /**
+   * @method init
+   * @description
+   *   Call the init method on each module.
+   *
+   * @return {Core} this for chaining
+   * @api public
+   */
+
+  Core.prototype.init = function(){
+    if (this.status === 'stopped') {
+      this.emit('pre init');
+      var modules = this.modules;
+      for (var module in modules) {
+        var initFn = modules[module].init;
+        if (modules.hasOwnProperty(module) && 'function' === typeof initFn) {
+          var mod = modules[module];
+          mod.init();
+        }
+      }
+      this.status = 'running';
+      this.emit('init');
+    }
+    return this;
+  };
+
+  /**
+   * @method use
+   * @description
+   *   Add a `module` to `this.modules`.
+   *
+   * @return {Core} this for chaining
+   * @api public
+   */
+
+  Core.prototype.use = function(module){
+    var args = [].slice.call(arguments);
+    if (args.length > 1) {
+      module = args[1];
+      module.id = args[0];
+    }
+    this.modules[module.id] = module;
+    return this;
+  };
+
+  /**
+   * @method create
+   * @description
+   *   Create a `Core`.
+   *
+   * @return {Core} new `Core`
+   * @api public
+   */
+
+  Core.create = function(id){
+    return new Core(id);
+  };
+
+  /*
+   * Expose `Core`
+   */
+
+  return Core;
+}));
